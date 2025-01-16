f740a771c9c1a71aa22263f77a65687c50b513d1
SesifredoDev <93771023+SesifredoDev@users.noreply.github.com>
2025-01-16 12:19:20 +0000

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SFULNONA.mjs";import"./chunk-E7E4ZPYQ.mjs";import"./chunk-WOZU2FWZ.mjs";import"./chunk-CX25AUUQ.mjs";import"./chunk-WYFJIWEB.mjs";import"./chunk-DHJQBDRG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
