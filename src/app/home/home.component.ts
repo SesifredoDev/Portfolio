916befe82770760463dbcdaf11d2a4ae8f4f35de
SesifredoDev <93771023+SesifredoDev@users.noreply.github.com>
2025-01-19 21:20:13 +0000

    home routerlink

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,11 @@ import {MatCardModule} from '@angular/material/card';
 import { NgFor } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, NgFor, MatButtonModule, MatIconModule],
+  imports: [MatGridListModule, MatCardModule, NgFor, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
